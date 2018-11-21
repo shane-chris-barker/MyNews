c62f88d2f32e7c9a99648aa79bedaad2a26c3589
Shane Chris Barker <geekychav@gmail.com>
2018-11-21 18:29:15 +0000

    [refs #10] removing style sheet as not used adding in full documentation

--- my_news_helper.php
+++ my_news_helper.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * Carry out generic functions related to the plugin
+ *
+ * Class does dirty tasks such as returning various config options.
+ *
+ * @since 1.0.0
+ */
 class My_News_Helper
 {
+    /**
+     * Get options to build select elements with.
+     *
+     * Function returns array with $key => $value pairs for
+     * populating the select elements on the admin page
+     *
+     * @since 1.0.0
+     *
+     * @param string $type The type of options array to build.
+     * @return array $key => $value array of options.
+     */
     function get_available_options($type)
     {
         switch($type) {
@@ -32,11 +50,4 @@ class My_News_Helper
         }
         return $options;
     }
-
-   function set_defaults()
-   {
-
-   }
-
-
  }
